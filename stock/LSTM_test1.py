c76a9de4802273792ed9843b93e2e0ff710e1020
James <james868198>
2019-03-11 20:57:43 -0700

    [Add&Modify]

--- stock/LSTM_test1.py
+++ stock/LSTM_test1.py
@@ -7,30 +7,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 apple = pd.read_csv('../data/transactions/Tech/AAPL.csv')
+google = pd.read_csv('../data/transactions/Tech/GOOG.csv')
 
 train_size = len(apple)*3//4
 test_size = len(apple) - train_size
-input_size = 1
-output_size = 1
-time_steps = 5
+time_steps = 15
 cell_unit = 16
-
-print(len(apple))
+epochNum = 100
 # open_train = np.array(sp500['Open'][0:train_size])
 # close_train = np.array(sp500['Close'][0:train_size])
 
 # open_test = np.array(sp500['Open'][train_size:])
 # cloase_test = np.array(sp500['Close'][train_size:])
-cl =  np.array(apple['Close'])
+apple_t =  np.array(apple['Close'])
+apple_t2 = np.array(apple['Close'])
+# apple_t = (apple_t-np.mean(apple_t))/(np.max(apple_t)-np.min(apple_t))
+# apple_t = apple_t/100
 
-cl = (cl-np.mean(cl))/(np.max(cl)-np.min(cl))
+google_t =  np.array(google['Close'])
+# google_t = (google_t-np.mean(google_t))/(np.max(google_t)-np.min(google_t))
+# google_t = google_t/100
+
+
 def processData(data,lb):
     X,Y = [],[]
     for i in range(len(data)-lb-1):
         X.append(data[i:(i+lb)])
         Y.append(data[(i+lb)])
     return np.array(X),np.array(Y)
-X,y = processData(cl,10)
+X,y = processData(apple_t,time_steps)
+apple_t2_i,apple_t2_o = processData(apple_t2,time_steps)
+apple_t2_i = apple_t2_i.reshape((apple_t2_i.shape[0],apple_t2_i.shape[1],1))
+
 X_train,X_test = X[:int(X.shape[0]*0.80)],X[int(X.shape[0]*0.80):]
 y_train,y_test = y[:int(y.shape[0]*0.80)],y[int(y.shape[0]*0.80):]
 
@@ -39,6 +47,10 @@ X_test = X_test.reshape((X_test.shape[0],X_test.shape[1],1))
 y_train = y_train.reshape((y_train.shape[0],1))
 y_test = y_test.reshape((y_test.shape[0],1))
 
+google_input,google_out = processData(google_t,time_steps)
+google_input = google_input.reshape((google_input.shape[0],google_input.shape[1],1))
+
+print(google_input.shape)
 
 print(X_train.shape)
 print(X_test.shape)
@@ -46,14 +58,10 @@ print(y_train.shape)
 print(y_test.shape)
 
 model = Sequential() # most common
-model.add(LSTM(cell_unit, input_shape = (10,1), activation = 'relu'))
-# model.add(Dropout(0.2))
+model.add(LSTM(cell_unit, input_shape = (time_steps,1), activation = 'relu'))
 
-# model.add(LSTM(cell_unit, activation = 'relu'))
-# model.add(Dropout(0.2))
-
-# model.add(Dense(time_steps, activation = 'relu'))
-# model.add(Dropout(0.2))
+model.add(Dense(time_steps, activation = 'relu'))
+model.add(Dropout(0.2))
 
 model.add(Dense(1))
 
@@ -63,12 +71,16 @@ model.compile(optimizer=apt, loss='mse')
 # print(X_train.shape[0],X_test.shape[1])
 
 callback = EarlyStopping(monitor='val_loss', patience=10,verbose=1,mode='auto')
-history = model.fit(X_train,y_train, epochs = 300, validation_data=(X_test,y_test),shuffle=False,callbacks =[callback])
+history = model.fit(X_train,y_train, epochs = epochNum, validation_data=(X_test,y_test),shuffle=False,callbacks =[callback])
 
 
-predictions = model.predict([X_test])
+print(apple_t2_i)
+print(apple_t2_o)
+predictions = model.predict([apple_t2_i])
 print(predictions[0])
-print(y_test[0])
-plt.plot(np.array(predictions))
-plt.plot(y_test)
+
+predictions = np.array(predictions)
+real = apple_t2_o
+plt.plot(predictions,color='green')
+plt.plot(real,color='red')
 plt.show()
